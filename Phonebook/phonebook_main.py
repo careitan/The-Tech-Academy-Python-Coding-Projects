e528c67e99eac35b0003e05f0f54ebfeb9c64821
Allan Reitan <careitan2000@gmail.com>
2019-02-06 18:08:16 -0800

    Initial build of files
    Initial build and creation of files.

---
+++ Phonebook/phonebook_main.py
@@ -0,0 +1,32 @@
+#   Python Ver:     3.7
+#
+#   Author:         Allan Reitan
+#
+#   Purpose:        Phonebook Demo.  Demonstrating OOP, Tkinter GUI module,
+#                   using Tkinter Parent and Child relationships.
+#
+#   Tested OS:      This code was written and tested to work with Windows 10.
+
+from tkinter import *
+import tkinter as tk
+
+
+#   Be sure to import our other modules
+#   so we can have access to them
+import phonebook_gui
+import phonebook_func
+
+
+#   Frame is the Tkinter frame class that our own class will inherit from
+class ParentWindow(Frame):
+    def __init__(self, master, *args, **kwargs):
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+
