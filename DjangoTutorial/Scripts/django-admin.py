264a013c03323f97c600e1dc6905a129b0918ad2
Allan Reitan <careitan2000@gmail.com>
2019-02-28 14:00:56 -0800

    Inital Build of Django Server
    Initial Build of Django Server files.

---
+++ DjangoTutorial/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\carei\documents\github\the-tech-academy-python-coding-projects\djangotutorial\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
